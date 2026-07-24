a86642b9a1422dda4803045ef68267998979eaf8
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-07-24 12:00:08 +0200

    tmwr -> chapter 12 ready to read (11_tuning.qmd)

---
+++ 11_tuning.R
@@ -0,0 +1,9 @@
+# Tidy Modeling With R
+# Model Tuning and the Dangers of Overfitting
+# https://www.tmwr.org/tuning
+
+# Packages ----
+
+library(tidyverse)
+library(tidymodels)
+tidymodels_prefer()
